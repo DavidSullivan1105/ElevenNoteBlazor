4087ecf65c67b34d24ff2cabe49ea46593ac9f15
David Sullivan <dcsulliv@gmail.com>
2022-10-11 20:32:19 -0400

    Services structure

---
+++ ElevenNoteWebApp/Server/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace ElevenNoteWebApp.Server.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }    
+    }
+}
